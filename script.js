38e7fb246fed2d11b74341d744c43464fb71cfe2
WaleedNasirAlsiraj <107930810+WaleedNasirAlsiraj@users.noreply.github.com>
2022-07-29 01:09:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function rangeSlide(value){
+    document.getElementById('range-value').innerHTML = value;
+}
+function rangeSlideWeight(value){
+    document.getElementById('range-value-weight').innerHTML = value;
+}
+function rangeSlideAge(value){
+    document.getElementById('range-value-age').innerHTML = value;
+}
